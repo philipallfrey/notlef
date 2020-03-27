fa67205de4e9dd610ec1dc0d5c7dde461633a93c
Philip Allfrey <philip@allfrey.co.nz>
2020-03-27 20:35:56 +1300

    Add text to overview, create colophon skeleton

---
+++ src/app/views/colophon-view/colophon-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colophon-view',
+  templateUrl: './colophon-view.component.html',
+  styleUrls: ['./colophon-view.component.css']
+})
+export class ColophonViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
